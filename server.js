28aa5bc1bd5f79c70febb686f55e3384672440e8
caio felipe <fcaio8966@gmail.com>
2025-06-30 20:41:09 -0300

    att

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express")
 
-const cursoCOntroller = require("./src/controllers/controllers.js")
+const cursoCOntroller = require("./src/controllers/cursosControllers.js")
 const app = express()
 
 
-app.get("/cursos", controlles.js.listar)
+app.get("/cursos", cursosControlles.listar)
 
 
 app.listen(3000, () => {
