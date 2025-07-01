0daa7e38dc5e182e09ca1922292a8ed1bd745c9d
caio felipe <fcaio8966@gmail.com>
2025-07-01 20:41:28 -0300

    att

--- src/controllers/cursosControllers.js
+++ src/controllers/cursosControllers.js
@@ -1,3 +1,11 @@
+const bancoDados = require("../config/banco-dados")
+
+async function listar(req,res){
+    const cursos =await bancoDados.conexao.query("SELECT*FROM cursos")
+    res.send("função responsavel pela listagem de professores")
+    console.log("curso",cursos)
+}
+
 function listar(req, res) {
     res.send("função listar");
 }
